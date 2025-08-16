982bb204779c80025526debbe13c7ce2b35bb52a
Thomas Ashwin Varghese <66424056+ashwinthomas143@users.noreply.github.com>
2025-08-16 01:03:50 -0400

    Delete assets/js directory

--- assets/js/search.js
+++
@@ -1,7 +0,0 @@
-SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: '/search.json',
-  searchResultTemplate: '<li><a href="{url}" class="text-blue-600 dark:text-blue-400">{title}</a></li>',
-  noResultsText: '<li>No results found</li>'
-});
